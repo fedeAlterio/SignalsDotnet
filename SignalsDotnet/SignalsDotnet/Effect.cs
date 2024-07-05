c7c22ef3a86dbb3671e078875b03fa077568914d
fedefex <48481385+fedeAlterio@users.noreply.github.com>
2024-07-05 20:48:33 +0200

    Effects

---
+++ SignalsDotnet/SignalsDotnet/Effect.cs
@@ -0,0 +1,20 @@
+﻿using System.Reactive;
+using System.Reactive.Concurrency;
+
+namespace SignalsDotnet;
+public class Effect : IDisposable
+{
+    readonly IDisposable _subscription;
+    public Effect(Action onChange, IScheduler? scheduler = null)
+    {
+        scheduler ??= DefaultScheduler;
+        _subscription = Signal.ComputedObservable(() =>
+        {
+            onChange();
+            return Unit.Default;
+        }, scheduler).Subscribe();
+    }
+    public static IScheduler? DefaultScheduler { get; set; }
+
+    public void Dispose() => _subscription.Dispose();
+}
--- SignalsDotnet/SignalsDotnet/Signal_Computed.cs
+++ SignalsDotnet/SignalsDotnet/Signal_Computed.cs
@@ -1,4 +1,7 @@
-﻿using System.Reactive.Disposables;
+﻿using System;
+using System.Reactive;
+using System.Reactive.Concurrency;
+using System.Reactive.Disposables;
 using System.Reactive.Linq;
 using SignalsDotnet.Configuration;
 using SignalsDotnet.Internals;
@@ -8,29 +11,6 @@ namespace SignalsDotnet;
 
 public partial class Signal
 {
-    internal static IReadOnlySignal<T> Computed<T>(Func<T> func, Func<Optional<T>> fallbackValue, ReadonlySignalConfigurationDelegate<T?>? configuration)
-    {
-        var valueObservable = Observable.Create<T>(observer =>
-        {
-            var disposable = new SerialDisposable();
-            OnNewResult(ComputeResult(func, fallbackValue));
-            void OnNewResult(ComputationResult<T> result)
-            {
-                disposable.Disposable = result.NextResult
-                                              .Take(1)
-                                              .Subscribe(OnNewResult);
-
-                // We notify a new value only if the func() evaluation succeeds.
-                if (result.ResultOptional.TryGetValue(out var propertyValue))
-                    observer.OnNext(propertyValue);
-            }
-
-            return disposable;
-        });
-
-        return new FromObservableSignal<T>(valueObservable, configuration)!;
-    }
-
     public static IReadOnlySignal<T> Computed<T>(Func<T> func, Func<T> fallbackValue, ReadonlySignalConfigurationDelegate<T?>? configuration = null)
     {
         if (func is null)
@@ -50,8 +30,49 @@ public partial class Signal
         return Computed(func, static () => Optional<T>.Empty, configuration);
     }
 
+    internal static IObservable<T> ComputedObservable<T>(Func<T> func, IScheduler? scheduler = null)
+    {
+        return ComputedObservable(func, static () => Optional<T>.Empty, scheduler);
+    }
 
-    static ComputationResult<T> ComputeResult<T>(Func<T> resultFunc, Func<Optional<T>> fallbackValue)
+    internal static IObservable<T> ComputedObservable<T>(Func<T> func, Func<T> fallbackValue, IScheduler? scheduler = null)
+    {
+        if (func is null)
+            throw new ArgumentNullException(nameof(func));
+
+        return ComputedObservable(func, () => new(fallbackValue()), scheduler);
+    }
+
+    internal static IReadOnlySignal<T> Computed<T>(Func<T> func, Func<Optional<T>> fallbackValue, ReadonlySignalConfigurationDelegate<T?>? configuration)
+    {
+        var valueObservable = ComputedObservable(func, fallbackValue, ImmediateScheduler.Instance);
+
+        return new FromObservableSignal<T>(valueObservable, configuration)!;
+    }
+
+    internal static IObservable<T> ComputedObservable<T>(Func<T> func, Func<Optional<T>> fallbackValue, IScheduler? scheduler = null)
+    {
+        return Observable.Create<T>(observer =>
+        {
+            var disposable = new SerialDisposable();
+            OnNewResult(ComputeResult(func, fallbackValue, scheduler));
+            void OnNewResult(ComputationResult<T> result)
+            {
+                disposable.Disposable = result.NextResult
+                                              .Take(1)
+                                              .Subscribe(OnNewResult);
+
+                // We notify a new value only if the func() evaluation succeeds.
+                if (result.ResultOptional.TryGetValue(out var propertyValue))
+                    observer.OnNext(propertyValue);
+            }
+
+            return disposable;
+        });
+    }
+
+
+    static ComputationResult<T> ComputeResult<T>(Func<T> resultFunc, Func<Optional<T>> fallbackValue, IScheduler? scheduler)
     {
         var referenceEquality = ReferenceEqualityComparer.Instance;
         HashSet<IReadOnlySignal> propertiesRequested = new(referenceEquality);
@@ -70,11 +91,18 @@ public partial class Signal
             }
         }
 
-        var observable = WhenAnyChanged(propertiesRequested)
-                         .Skip(1) // We want to observe only future changes
-                         .Select(_ => ComputeResult(resultFunc, fallbackValue));
 
-        return new(observable, result);
+        var observable = WhenAnyChanged(propertiesRequested).Skip(1); // We want to observe only future changes
+
+        if (scheduler is not null)
+        {
+            observable = observable.Select(x => Observable.Return(x, scheduler))
+                                   .Switch();
+        }
+
+        var restulObsrvable = observable.Select(_ => ComputeResult(resultFunc, fallbackValue, scheduler));
+
+        return new(restulObsrvable, result);
     }
 
     record struct ComputationResult<T>(IObservable<ComputationResult<T>> NextResult, Optional<T> ResultOptional);
