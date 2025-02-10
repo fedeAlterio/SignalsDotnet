dc86303dcaccc1b9537165a6962da32b7981f8d7
Federico Alterio <DID-31@systemceramics.com>
2025-02-10 19:40:20 +0100

    Reactive animations

---
+++ SignalsDotnet/SignalsDotnet/Animations/Animation.cs
@@ -0,0 +1,181 @@
+﻿using System.Diagnostics;
+using System.Drawing;
+using System.Numerics;
+using R3;
+
+namespace SignalsDotnet.Animations;
+
+public static class ReactiveAnimation
+{
+    public static Observable<AnimationFrame<double>> WithDuration(TimeSpan duration, FrameProvider? frameProvider = null)
+    {
+        return Observable.Defer(() =>
+        {
+            var stopWatch = Stopwatch.StartNew();
+            TimeSpan elapsed = TimeSpan.Zero;
+            const double from = 0;
+            const double to = 1;
+            var speed = 1 / duration.TotalMilliseconds;
+
+            AnimationFrame<double> GetAnimationFrame(double previousValue)
+            {
+                var previousElapsed = elapsed;
+                elapsed = stopWatch.Elapsed;
+                var deltaTime = elapsed - previousElapsed;
+                var newValue = previousValue + speed * deltaTime.TotalMilliseconds;
+                return new(newValue, deltaTime);
+            }
+
+            stopWatch.Start();
+
+            var everyUpdate = frameProvider is null ? Observable.EveryUpdate() : Observable.EveryUpdate(frameProvider);
+
+            return everyUpdate.Scan(new AnimationFrame<double>(from, TimeSpan.Zero), (x, _) => GetAnimationFrame(x.Value))
+                              .TakeUntil(x => x.Value is <= @from or >= to)
+                              .Do(onDispose: stopWatch.Stop);
+        });
+    }
+
+    public static Observable<AnimationFrame<double>> Animate(this Observable<AnimationFrame<double>> @this,
+                                                             Action<AnimationFrame<double>> action,
+                                                             double from = 0,
+                                                             double to = 1,
+                                                             AnimationCancellationOptions animationCancellationOptions = AnimationCancellationOptions.KeepLastValue)
+    {
+        return Observable.Create<AnimationFrame<double>>(observer =>
+        {
+            Stopwatch? stopWatch = animationCancellationOptions == AnimationCancellationOptions.KeepLastValue
+                ? null
+                : Stopwatch.StartNew();
+
+            double lastValue = 0;
+
+            var singleAssignmentDisposable = new SingleAssignmentDisposable();
+            var disposable = @this.Do(x =>
+                                  {
+                                      var min = Math.Min(from, to);
+                                      var max = Math.Max(from, to);
+                                      var value = Math.Clamp(from + x.Value * (to - from), min, max);
+                                      lastValue = value;
+                                      stopWatch?.Reset();
+                                      action(x with { Value = value });
+                                  })
+                                  .Subscribe(observer.OnNext, observer.OnErrorResume, observer.OnCompleted);
+
+            singleAssignmentDisposable.Disposable = disposable;
+
+            return Disposable.Create(() =>
+            {
+                try
+                {
+                    if (animationCancellationOptions is not AnimationCancellationOptions.KeepLastValue)
+                    {
+                        stopWatch!.Stop();
+                        if (lastValue == to)
+                        {
+                            return;
+                        }
+
+                        var elapsed = stopWatch!.Elapsed;
+                        var value = animationCancellationOptions is AnimationCancellationOptions.SnapToStart ? from : to;
+                        action(new AnimationFrame<double>(value, elapsed));
+                    }
+                }
+                finally
+                {
+                    singleAssignmentDisposable.Dispose();
+                }
+            });
+        });
+    }
+
+    public static Observable<AnimationFrame<double>> Animate(this Observable<AnimationFrame<double>> @this,
+                                                             Action<AnimationFrame<Vector4>> action,
+                                                             Vector4 from,
+                                                             Vector4 to,
+                                                             AnimationCancellationOptions animationCancellationOptions = AnimationCancellationOptions.KeepLastValue)
+    {
+        var deltaPos = to - from;
+        return @this.Animate(x =>
+        {
+            var animationFrame = new AnimationFrame<Vector4>
+            {
+                DeltaTime = x.DeltaTime,
+                Value = from + (float)x.Value * deltaPos
+            };
+
+            action(animationFrame);
+        }, 0, 1, animationCancellationOptions);
+    }
+
+
+    public static Observable<AnimationFrame<double>> Animate(this Observable<AnimationFrame<double>> @this,
+                                                             Action<AnimationFrame<Vector3>> action,
+                                                             Vector3 from,
+                                                             Vector3 to,
+                                                             AnimationCancellationOptions animationCancellationOptions = AnimationCancellationOptions.KeepLastValue)
+    {
+        var deltaPos = to - from;
+        return @this.Animate(x =>
+        {
+            var animationFrame = new AnimationFrame<Vector3>
+            {
+                DeltaTime = x.DeltaTime,
+                Value = from + (float)x.Value * deltaPos
+            };
+
+            action(animationFrame);
+        }, 0, 1, animationCancellationOptions);
+    }
+
+
+    public static Observable<AnimationFrame<double>> Animate(this Observable<AnimationFrame<double>> @this,
+                                                             Action<AnimationFrame<Vector2>> action,
+                                                             Vector2 from,
+                                                             Vector2 to,
+                                                             AnimationCancellationOptions animationCancellationOptions = AnimationCancellationOptions.KeepLastValue)
+    {
+        var deltaPos = to - from;
+        return @this.Animate(x =>
+        {
+            var animationFrame = new AnimationFrame<Vector2>
+            {
+                DeltaTime = x.DeltaTime,
+                Value = from + (float)x.Value * deltaPos
+            };
+
+            action(animationFrame);
+        }, 0, 1, animationCancellationOptions);
+    }
+
+    public static Observable<AnimationFrame<double>> Animate(this Observable<AnimationFrame<double>> @this,
+                                                             Action<AnimationFrame<Color>> action,
+                                                             Color from,
+                                                             Color to,
+                                                             AnimationCancellationOptions animationCancellationOptions = AnimationCancellationOptions.KeepLastValue)
+    {
+        return @this.Animate(x =>
+        {
+            var value = x.Value;
+            var animationFrame = new AnimationFrame<Color>
+            {
+                DeltaTime = x.DeltaTime,
+                Value = Color.FromArgb((int)(from.A + (to.A - from.A) * value),
+                                       (int)(from.R + (to.R - from.R) * value),
+                                       (int)(from.G + (to.G - from.G) * value),
+                                       (int)(from.B + (to.B - from.B) * value))
+            };
+
+            action(animationFrame);
+        }, 0, 1, animationCancellationOptions);
+    }
+}
+
+public enum AnimationCancellationOptions
+{
+    KeepLastValue,
+    SnapToStart,
+    SnapToEnd
+}
+
+public record struct AnimationFrame<T>(T Value, TimeSpan DeltaTime);
---
+++ SignalsDotnet/SignalsDotnet/Animations/AnimationEasings.cs
@@ -0,0 +1,218 @@
+﻿using R3;
+
+namespace SignalsDotnet.Animations;
+
+public static class AnimationEasings
+{
+    public static Observable<AnimationFrame<double>> WithEasingFunction(this Observable<AnimationFrame<double>> @this, Func<double, double> easingFunction)
+    {
+        return @this.Select(x => x with { Value = easingFunction(x.Value) });
+    }
+
+    public static Observable<AnimationFrame<double>> WithEaseInSine(this Observable<AnimationFrame<double>> @this)
+    {
+        return @this.WithEasingFunction(static x => 1 - Math.Cos(x * Math.PI / 2));
+    }
+
+    public static Observable<AnimationFrame<double>> WithEaseOutSine(this Observable<AnimationFrame<double>> @this)
+    {
+        return @this.WithEasingFunction(static x => Math.Sin((x * Math.PI) / 2));
+    }
+
+    public static Observable<AnimationFrame<double>> WithEaseInOutSine(this Observable<AnimationFrame<double>> @this)
+    {
+        return @this.WithEasingFunction(static x => -(Math.Cos(Math.PI * x) - 1) / 2);
+    }
+
+    public static Observable<AnimationFrame<double>> WithEaseInQuad(this Observable<AnimationFrame<double>> @this)
+    {
+        return @this.WithEasingFunction(static x => x * x);
+    }
+
+    public static Observable<AnimationFrame<double>> WithEaseOutQuad(this Observable<AnimationFrame<double>> @this)
+    {
+        return @this.WithEasingFunction(static x => 1 - (1 - x) * (1 - x));
+    }
+
+    public static Observable<AnimationFrame<double>> WithEaseInOutQuad(this Observable<AnimationFrame<double>> @this)
+    {
+        return @this.WithEasingFunction(static x => x < 0.5
+                                                 ? 2 * x * x
+                                                 : 1 - Math.Pow(-2 * x + 2, 2) / 2);
+    }
+
+    public static Observable<AnimationFrame<double>> WithEaseInCubic(this Observable<AnimationFrame<double>> @this)
+    {
+        return @this.WithEasingFunction(static x => x * x * x);
+    }
+
+    public static Observable<AnimationFrame<double>> WithEaseOutCubic(this Observable<AnimationFrame<double>> @this)
+    {
+        return @this.WithEasingFunction(static x => 1 - Math.Pow(1 - x, 3));
+    }
+
+    public static Observable<AnimationFrame<double>> WithEaseInOutCubic(this Observable<AnimationFrame<double>> @this)
+    {
+        return @this.WithEasingFunction(static x => x < 0.5
+                                                 ? 4 * x * x * x
+                                                 : 1 - Math.Pow(-2 * x + 2, 3) / 2);
+    }
+
+    public static Observable<AnimationFrame<double>> WithEaseInQuart(this Observable<AnimationFrame<double>> @this)
+    {
+        return @this.WithEasingFunction(static x => x * x * x * x);
+    }
+
+    public static Observable<AnimationFrame<double>> WithEaseOutQuart(this Observable<AnimationFrame<double>> @this)
+    {
+        return @this.WithEasingFunction(static x => 1 - Math.Pow(1 - x, 4));
+    }
+
+    public static Observable<AnimationFrame<double>> WithEaseInOutQuart(this Observable<AnimationFrame<double>> @this)
+    {
+        return @this.WithEasingFunction(static x => x < 0.5
+                                                 ? 8 * x * x * x * x
+                                                 : 1 - Math.Pow(-2 * x + 2, 4) / 2);
+    }
+
+    public static Observable<AnimationFrame<double>> WithEaseInQuint(this Observable<AnimationFrame<double>> @this)
+    {
+        return @this.WithEasingFunction(static x => x * x * x * x * x);
+    }
+
+    public static Observable<AnimationFrame<double>> WithEaseOutQuint(this Observable<AnimationFrame<double>> @this)
+    {
+        return @this.WithEasingFunction(static x => 1 - Math.Pow(1 - x, 5));
+    }
+
+    public static Observable<AnimationFrame<double>> WithEaseInOutQuint(this Observable<AnimationFrame<double>> @this)
+    {
+        return @this.WithEasingFunction(static x => x < 0.5
+                                                 ? 16 * x * x * x * x * x
+                                                 : 1 - Math.Pow(-2 * x + 2, 5) / 2);
+    }
+
+    public static Observable<AnimationFrame<double>> WithEaseInExpo(this Observable<AnimationFrame<double>> @this)
+    {
+        return @this.WithEasingFunction(static x => x == 0 ? 0 : Math.Pow(2, 10 * x - 10));
+    }
+
+    public static Observable<AnimationFrame<double>> WithEaseOutExpo(this Observable<AnimationFrame<double>> @this)
+    {
+        return @this.WithEasingFunction(static x => x == 1 ? 1 : 1 - Math.Pow(2, -10 * x));
+    }
+
+    public static Observable<AnimationFrame<double>> WithEaseInOutExpo(this Observable<AnimationFrame<double>> @this)
+    {
+        return @this.WithEasingFunction(static x => 
+                                             x == 0 ? 0 :
+                                             x == 1 ? 1 :
+                                             x < 0.5 ? Math.Pow(2, 20 * x - 10) / 2
+                                             : (2 - Math.Pow(2, -20 * x + 10)) / 2);
+    }
+
+    public static Observable<AnimationFrame<double>> WithEaseInCirc(this Observable<AnimationFrame<double>> @this)
+    {
+        return @this.WithEasingFunction(static x => 1 - Math.Sqrt(1 - Math.Pow(x, 2)));
+    }
+
+    public static Observable<AnimationFrame<double>> WithEaseOutCirc(this Observable<AnimationFrame<double>> @this)
+    {
+        return @this.WithEasingFunction(static x => Math.Sqrt(1 - Math.Pow(x - 1, 2)));
+    }
+
+    public static Observable<AnimationFrame<double>> WithEaseInOutCirc(this Observable<AnimationFrame<double>> @this)
+    {
+        return @this.WithEasingFunction(static x =>
+                                             x < 0.5
+                                                 ? (1 - Math.Sqrt(1 - Math.Pow(2 * x,  2))) / 2
+                                                 : (Math.Sqrt(1 - Math.Pow(-2 * x + 2, 2)) + 1) / 2);
+    }
+
+    public static Observable<AnimationFrame<double>> WithEaseInBack(this Observable<AnimationFrame<double>> @this)
+    {
+        const double c1 = 1.70158;
+        const double c3 = c1 + 1;
+
+        return @this.WithEasingFunction(static x => c3 * x * x * x - c1 * x * x);
+    }
+
+    public static Observable<AnimationFrame<double>> WithEaseOutBack(this Observable<AnimationFrame<double>> @this)
+    {
+        const double c1 = 1.70158;
+        const double c3 = c1 + 1;
+
+        return @this.WithEasingFunction(static x => 1 + c3 * Math.Pow(x - 1, 3) + c1 * Math.Pow(x - 1, 2));
+    }
+
+    public static Observable<AnimationFrame<double>> WithEaseInOutBack(this Observable<AnimationFrame<double>> @this)
+    {
+        const double c1 = 1.70158;
+        const double c2 = c1 * 1.525;
+
+        return @this.WithEasingFunction(static x => x < 0.5
+                                                 ? (Math.Pow(2 * x, 2) * ((c2 + 1) * 2 * x - c2)) / 2
+                                                 : (Math.Pow(2 * x - 2, 2) * ((c2 + 1) * (x * 2 - 2) + c2) + 2) / 2);
+    }
+
+    public static Observable<AnimationFrame<double>> WithEaseInElastic(this Observable<AnimationFrame<double>> @this)
+    {
+        const double c4 = (2 * Math.PI) / 3;
+
+        return @this.WithEasingFunction(static x => x == 0 ? 0
+                                                 : x == 1 ? 1
+                                                 : -Math.Pow(2, 10 * x - 10) * Math.Sin((x * 10 - 10.75) * c4));
+    }
+
+    public static Observable<AnimationFrame<double>> WithEaseOutElastic(this Observable<AnimationFrame<double>> @this)
+    {
+        const double c4 = (2 * Math.PI) / 3;
+
+        return @this.WithEasingFunction(static x => x switch
+        {
+            0 => 0,
+            1 => 1,
+            _ => Math.Pow(2, -10 * x) * Math.Sin((x * 10 - 0.75) * c4) + 1
+        });
+    }
+
+    public static Observable<AnimationFrame<double>> WithEaseInOutElastic(this Observable<AnimationFrame<double>> @this)
+    {
+        const double c5 = (2 * Math.PI) / 4.5;
+
+        return @this.WithEasingFunction(static x => x == 0 ? 0
+                                                 : x == 1 ? 1
+                                                 : x < 0.5 ? -(Math.Pow(2, 20 * x - 10) * Math.Sin((20 * x - 11.125) * c5)) / 2
+                                                 : (Math.Pow(2,  -20 * x + 10) * Math.Sin((20 * x - 11.125) * c5)) / 2 + 1);
+    }
+
+    public static Observable<AnimationFrame<double>> WithEaseOutBounce(this Observable<AnimationFrame<double>> @this)
+    {
+        return @this.WithEasingFunction(EaseOutBounceFunction);
+    }
+
+    static double EaseOutBounceFunction(double x)
+    {
+        const double n1 = 7.5625;
+        const double d1 = 2.75;
+        return x switch
+        {
+            < 1 / d1 => n1 * x * x,
+            < 2 / d1 => n1 * (x -= 1.5 / d1) * x + 0.75,
+            < 2.5 / d1 => n1 * (x -= 2.25 / d1) * x + 0.9375,
+            _ => n1 * (x -= 2.625 / d1) * x + 0.984375
+        };
+    }
+
+    public static Observable<AnimationFrame<double>> WithEaseInOutBounce(this Observable<AnimationFrame<double>> @this)
+    {
+        return @this.WithEasingFunction(static x => x < 0.5
+                                                 ? (1 - EaseOutBounceFunction(1 - 2 * x)) / 2
+                                                 : (1 + EaseOutBounceFunction(2 * x - 1)) / 2);
+    }
+
+    public static Observable<AnimationFrame<double>> WithEaseInBounce(this Observable<AnimationFrame<double>> @this)
+    {
+        return @this.WithEasingFunction(x => 1 - EaseOutBounceFunction(x));
+    }
+}
