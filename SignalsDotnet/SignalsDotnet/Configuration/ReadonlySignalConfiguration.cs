010d32cab0f31eae805d9ad092bb5cc19a6bace0
Federico Alterio <federico.alterio@mail.polimi.it>
2026-06-25 22:32:42 +0200

    refcount computed

--- SignalsDotnet/SignalsDotnet/Configuration/ReadonlySignalConfiguration.cs
+++ SignalsDotnet/SignalsDotnet/Configuration/ReadonlySignalConfiguration.cs
@@ -3,7 +3,8 @@
 public delegate ReadonlySignalConfiguration<T> ReadonlySignalConfigurationDelegate<T>(ReadonlySignalConfiguration<T> startConfiguration);
 public record ReadonlySignalConfiguration<T>(IEqualityComparer<T> Comparer,
                                              bool RaiseOnlyWhenChanged,
-                                             bool SubscribeWeakly)
+                                             bool SubscribeWeakly,
+                                             SubscriptionStrategy SubscriptionStrategy = SubscriptionStrategy.Persistent)
 {
-    public static ReadonlySignalConfiguration<T> Default { get; } = new(EqualityComparer<T>.Default, true, false);
+    public static ReadonlySignalConfiguration<T> Default { get; } = new(EqualityComparer<T>.Default, true, false, SubscriptionStrategy.Persistent);
 }
